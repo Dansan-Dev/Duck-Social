29b85bd0740f63af9930027de212de86ecddc82b
Daniel Sandstrom <github@danielsandstrom.org>
2025-05-16 11:43:40 +0200

    Refactored root to use RouterContext

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,12 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
+import {Outlet, createRootRouteWithContext} from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import type {AuthContext} from "@/hooks/useAuth.ts";
 
-export const Route = createRootRoute({
+type RouterContext = {
+    authentication: AuthContext;
+}
+
+export const Route = createRootRouteWithContext<RouterContext>()({
   component: () => (
     <>
       <Outlet />
